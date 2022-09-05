9b149de7f91011f40aab033b335b864c19cc597a
Saman <samanzahid012@gmail.com>
2022-09-05 17:54:04 +0500

    update

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ function calculate(){
     let table = document.querySelector("#table").value;
     document.querySelector("#result").innerHTML= ""; 
     
- for (let i=1; i<11; i++ ){
-     document.querySelector("#result").innerHTML+=` ${table} X ${i} = ${table * i} <br/>`;
+ for (let i=0; i<10; i++ ){
+     document.querySelector("#result").innerHTML+=` ${table} X ${i+1} = ${table * (i+1)} <br/>`;
  }
 }
